df5ff0a8f72aaf41db217479582e14b1d8d87d74
caseychoiniere <caseychoiniere@gmail.com>
2024-07-16 10:03:16 -0700

    Fix date sort

--- components/Event.tsx
+++ components/Event.tsx
@@ -1,53 +1,62 @@
-import React from "react";
-import Router from "next/router";
-import Image from "next/image";
-import {useSession} from "next-auth/react";
+import React from 'react'
+import Router from 'next/router'
+import Image from 'next/image'
+import { useSession } from 'next-auth/react'
 
 export type EventProps = {
-    cost?: string;
-    id: string;
-    title: string;
-    location: string;
-    description: string;
-    eventURL: string;
-    date: string;
-    time?: string;
-    image?: string;
+    cost?: string
+    id: string
+    title: string
+    location: string
+    description: string
+    eventURL: string
+    date: string
+    time?: string
+    image?: string
     createdBy: {
-        name: string;
-        email: string;
-    } | null;
-    published: boolean;
-};
-
-function formatDate(dateString: string): string {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    const date = new Date(dateString);
-    const dayOfWeek = days[date.getUTCDay()];
-    const month = date.getUTCMonth() + 1; // getUTCMonth returns 0-11
-    const day = date.getUTCDate();
-    const year = date.getUTCFullYear();
-
-    return `${dayOfWeek} ~ ${month}/${day}/${year}`;
+        name: string
+        email: string
+    } | null
+    published: boolean
 }
 
+function formatDate(dateString: string): string {
+    const days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+    ]
+    const date = new Date(dateString)
+    const dayOfWeek = days[date.getUTCDay()]
+    const month = date.getUTCMonth() + 1 // getUTCMonth returns 0-11
+    const day = date.getUTCDate()
+    const year = date.getUTCFullYear()
 
+    return `${dayOfWeek} ~ ${month}/${day}/${year}`
+}
 
-const Event: React.FC<{ event: EventProps }> = ({event}) => {
-    const {data: session} = useSession();
+const Event: React.FC<{ event: EventProps }> = ({ event }) => {
+    const { data: session } = useSession()
 
     async function deleteEvent(id: string): Promise<void> {
         await fetch(`/api/event/${id}`, {
             method: 'DELETE',
-        });
+        })
         Router.push('/')
     }
 
     return (
         <div id="card" className="">
             <div className="container w-100 lg:w-4/5 mx-auto flex flex-col">
-                <div v-for="card in cards" className="flex flex-col md:flex-row overflow-hidden
-                                        bg-slate-950 rounded-md shadow-xl  mt-4 w-100 mx-2 opacity-80">
+                <div
+                    v-for="card in cards"
+                    className="flex flex-col md:flex-row overflow-hidden
+                                        bg-slate-950 rounded-md shadow-xl  mt-4 w-100 mx-2 opacity-80"
+                >
                     <div className="w-auto md:w-1/2">
                         <Image
                             className="inset-0 h-full w-full object-cover object-center"
@@ -60,48 +69,48 @@ const Event: React.FC<{ event: EventProps }> = ({event}) => {
                         />
                     </div>
                     <div className="w-full py-4 px-6 text-white flex flex-col">
-                        <h3 className="font-semibold text-2xl text-wrap text-white leading-tight truncate">{event.title}</h3>
-                        <p className="mt-2 text-lg">
-                            {event.location}
-                        </p>
-                        <p className="mt-2 text-lg">
-                            {event.description}
-                        </p>
-                        <p className="mt-2 text-lg">
-                            {event.cost}
-                        </p>
-                        <p className="mt-2 text-lg">
-                            {event.time}
-                        </p>
-                        {event.eventURL && <a href={event.eventURL} target="_blank"
-                                              className="mt-2 text-lg underline text-blue-300">
-                          Event Page
-                        </a>}
+                        <h3 className="font-semibold text-2xl text-wrap text-white leading-tight truncate">
+                            {event.title}
+                        </h3>
+                        <p className="mt-2 text-lg">{event.location}</p>
+                        <p className="mt-2 text-lg">{event.description}</p>
+                        <p className="mt-2 text-lg">{event.cost}</p>
+                        <p className="mt-2 text-lg">{event.time}</p>
+                        {event.eventURL && (
+                            <a
+                                href={event.eventURL}
+                                target="_blank"
+                                className="mt-2 text-lg underline text-blue-300"
+                            >
+                                Event Page
+                            </a>
+                        )}
                         <p className="text-lg text-gray-700 text-white tracking-wide font-semibold mt-2">
                             {formatDate(event.date)}
                         </p>
-                        {session &&
+                        {session && (
                             <>
-                              <button
-                                  className="bg-red-600 p-3 rounded-md my-2"
-                                  onClick={() => deleteEvent(event.id)}
-                              >
-                                Delete
-                              </button>
-                              <button
-                                  className="bg-green-600 p-3 rounded-md my-2"
-                                  onClick={() => Router.push(`/create?id=${event.id}`)}
-                              >
-                                Edit Event
-                              </button>
+                                <button
+                                    className="bg-red-600 p-3 rounded-md my-2"
+                                    onClick={() => deleteEvent(event.id)}
+                                >
+                                    Delete
+                                </button>
+                                <button
+                                    className="bg-green-600 p-3 rounded-md my-2"
+                                    onClick={() =>
+                                        Router.push(`/create?id=${event.id}`)
+                                    }
+                                >
+                                    Edit Event
+                                </button>
                             </>
-                        }
+                        )}
                     </div>
                 </div>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Event;
-
+export default Event
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,69 +1,83 @@
-import React, {useEffect, useState} from "react"
-import prisma from '../lib/prisma';
+import React, { useEffect, useState } from 'react'
+import prisma from '../lib/prisma'
 import Image from 'next/image'
-import {useRouter} from 'next/router';
-import {GetStaticProps} from "next"
-import Layout from "../components/Layout"
-import Event, {EventProps} from "../components/Event"
-import Loading from "../components/Loading";
+import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
+import Layout from '../components/Layout'
+import Event, { EventProps } from '../components/Event'
+import Loading from '../components/Loading'
 
-function sortAndSeparateEvents(events: EventProps[]): [EventProps[], EventProps[]] {
-    const now = new Date();
-    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+function sortAndSeparateEvents(
+    events: EventProps[]
+): [EventProps[], EventProps[]] {
+    const now = new Date()
+    const today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
 
-    const getEventDay = (event: { date: string | number | Date; }) => {
-        const eventDate = new Date(event.date);
-        return new Date(eventDate.getFullYear(), eventDate.getMonth(), eventDate.getDate());
-    };
+    const getEventDay = (event: { date: string | number | Date }) => {
+        const eventDate = new Date(event.date)
+        return new Date(
+            eventDate.getFullYear(),
+            eventDate.getMonth(),
+            eventDate.getDate()
+        )
+    }
 
     const futureEvents = events
-        .filter(event => {
-            return getEventDay(event) >= today;
+        .filter((event) => {
+            const date = new Date().toISOString().split('T')[0]
+            const eventDate = new Date(event.date).setHours(0, 0, 0, 0)
+            return eventDate >= new Date(date).setHours(0, 0, 0, 0)
         })
-        .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+        .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
 
-    const pastEvents = events.filter(event => {
-        return getEventDay(event) < today;
-    });
+    const pastEvents = events.filter((event) => {
+        return getEventDay(event) < today
+    })
 
-    return [futureEvents, pastEvents];
+    return [futureEvents, pastEvents]
 }
 
-
 export const getStaticProps: GetStaticProps = async () => {
+    const today = new Date()
+    today.setHours(0, 0, 0, 0)
+
     let events = await prisma.event.findMany({
-        where: {published: true},
+        where: {
+            published: true,
+        },
         include: {
             createdBy: {
-                select: {name: true, email: true},
+                select: { name: true, email: true },
             },
         },
-    });
+    })
 
-    // Convert date fields to serializable format
     // @ts-ignore
-    events = events.map(event => ({
+    events = events.map((event) => ({
         ...event,
-        date: event.date.toISOString(),
-    }));
+        date: event.date.toISOString().split('T')[0],
+    }))
 
     return {
-        props: {events},
+        props: { events },
         revalidate: 10,
-    };
-};
+    }
+}
 
 type Props = {
     events: EventProps[]
 }
 
 const Home: React.FC<Props> = (props) => {
-    const router = useRouter();
-    const [loading, setLoading] = useState(false);
+    const router = useRouter()
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
-        const handleRouteChange = (url) => (url !== router.asPath) && setLoading(true);
-        const handleRouteComplete = (url) => (url === router.asPath) && setLoading(true);
+        const handleRouteChange = (url) =>
+            url !== router.asPath && setLoading(true)
+        const handleRouteComplete = (url) =>
+            url === router.asPath && setLoading(false)
+
         router.events.on('routeChangeStart', handleRouteChange)
         router.events.on('routeChangeComplete', handleRouteComplete)
         router.events.on('routeChangeError', handleRouteComplete)
@@ -75,34 +89,37 @@ const Home: React.FC<Props> = (props) => {
         }
     }, [])
 
-    const [futureEvents, pastEvents] = sortAndSeparateEvents(props.events);
+    const [futureEvents, pastEvents] = sortAndSeparateEvents(props.events)
 
     return (
         <Layout>
-            {loading ? <Loading/> :
-                (
-                    <>
-                        <div className="hero">
-                            <Image
-                                src="/ds_logo.png"
-                                style={{marginTop: 60}}
-                                width={200}
-                                height={100}
-                                alt="Dead Streets Wolf Logo"
-                            />
-                        </div>
-                        <div className="page">
-                            <main>
-                                <h1 className="upcoming-shows-header text-2xl">Upcoming Shows</h1>
-                                {futureEvents.map((event) => (
-                                    <div key={event.id} className="event">
-                                        <Event event={event}/>
-                                    </div>
-                                ))}
-                            </main>
-                        </div>
-                    </>
-                )}
+            {loading ? (
+                <Loading />
+            ) : (
+                <>
+                    <div className="hero">
+                        <Image
+                            src="/ds_logo.png"
+                            style={{ marginTop: 60 }}
+                            width={200}
+                            height={100}
+                            alt="Dead Streets Wolf Logo"
+                        />
+                    </div>
+                    <div className="page">
+                        <main>
+                            <h1 className="upcoming-shows-header text-2xl">
+                                Upcoming Shows
+                            </h1>
+                            {futureEvents.map((event) => (
+                                <div key={event.id} className="event">
+                                    <Event event={event} />
+                                </div>
+                            ))}
+                        </main>
+                    </div>
+                </>
+            )}
             <style jsx>{`
                 .hero {
                     background-size: cover;
@@ -158,4 +175,4 @@ const Home: React.FC<Props> = (props) => {
     )
 }
 
-export default Home;
+export default Home
